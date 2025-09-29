63139f2d55ebb3ed78521e7a8ae2069b673f343b
KikoNassif <ktamer2003@gmail.com>
2025-09-29 12:09:24 +0400

    Created Auth API's

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const connectDB = require("./config/db")
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes")
 
 const app = express();
 
@@ -18,6 +19,11 @@ app.use(
 app.use(express.json());
 
 connectDB();
+
+app.use("/api/v1/auth", authRoutes);
+
+// Serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
